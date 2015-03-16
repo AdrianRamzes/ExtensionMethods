fd359b3cab9ed0c48be001447293e0d7bfc8a517
ramzes1992 <adrian.ramzes@gmail.com>
2015-03-17 00:15:38 +0100

    Added files to track

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Universal;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            3.Times(() => Console.WriteLine("I Love C#"));
+
+            int.MaxValue.Times((i) => Console.WriteLine(i + " is " + (i % 2 == 0 ? i + "even" : "odd")));
+
+            int? someNullableObject = null;
+
+            if (someNullableObject.IsNull())
+            {
+                //TODO: something
+            }
+
+            object[] array = new object[5];
+            array.ForEach((o) => Console.WriteLine(o.IsNull() ? "null" : "not null"));
+
+            array.Length.Times((o) => Console.WriteLine(o.IsNull() ? "null" : "not null"));
+
+            "THE END".ForEach((c) => { });
+        }
+    }
+}
---
+++ Universal/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal
+{
+    public static class Extensions
+    {
+        public static void Times(this int count, Action action)
+        {
+            for(int i =0; i < count;i++)
+            {
+                action();
+            }
+        }
+
+        public static void Times(this int count, Action<int> action)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                action(i);
+            }
+        }
+
+        public static bool IsNull(this object o)
+        {
+            return o == null;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (T element in collection)
+            {
+                action(element);
+            }
+        }
+    }
+}
